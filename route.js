f988886a12b224f7ddca8a3ca3c7a80f11e60b8a
Marco Kotrotsos <maildmz@gmail.com>
2012-07-14 15:35:12 +0200

    removed example code from class

--- route.js
+++ route.js
@@ -39,15 +39,3 @@ Route.prototype = {
         }
 };
 
-var route = new Route();
-
-// Define a routing handler that respons to #foo/1/3.
-// this holds the router object for that particular match.
-route.define("#foo/1/3" , function () {
-        console.log( 'you got me from foo' , this );
-} );
-route.define("#bar/12" , function () {
-        console.log( 'you got me from bar' , this );
-} );
-// Router start.
-route.start();
